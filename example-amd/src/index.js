64e85cd555571860929ca7e152bda8e16b531cff
Shaoke <shaoke.xu@oracle.com>
2019-04-03 10:34:51 -0700

    Add example webpack

--- example-amd/src/index.js
+++ example-amd/src/index.js
@@ -1,8 +1,13 @@
-import './index.pcss';
-import MyApp from './components/my-app';
+// import './index.pcss';
+// import MyApp from './components/my-app';
 
-if (PRODUCTION) {
-  require('offline-plugin/runtime').install();
+// if (PRODUCTION) {
+//   require('offline-plugin/runtime').install();
+// }
+
+// customElements.define('my-app', MyApp);
+function bootstrap(){
+  console.log('bootstrap');
 }
 
-customElements.define('my-app', MyApp);
+bootstrap();
